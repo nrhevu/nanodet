afbf872c0cd3adf76e3a2b66e8fa8743e8c33fe9
Nguyen The Vu <vu.nguyen@moreh.com.vn>
2025-11-04 02:37:41 +0900

    update install

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
-from nanodet import __author__, __author_email__, __docs__, __homepage__, __version__
+# from nanodet import __author__, __author_email__, __docs__, __homepage__, __version__
+
+import time
+
+_this_year = time.strftime("%Y")
+__version__ = "1.0.0"
+__author__ = "RangiLyu"
+__author_email__ = "lyuchqi@gmail.com"
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright (c) 2020-{_this_year}, {__author__}."
+__homepage__ = "https://github.com/RangiLyu/nanodet"
+
+__docs__ = (
+    "NanoDet: Deep learning object detection toolbox for super fast and "
+    "lightweight anchor-free object detection models."
+)
+
 
 if __name__ == "__main__":
     setup(
